a195277d249592f43489f9e29ab4a67b8a485960
anlegent <alegent@edenschool.fr>
2024-04-09 16:12:53 +0200

    starting to make a possibility of choosing their own numbers

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
+// 
+// for (let i = 0; i < 20; i++) {
+// function getNumber(max) {
+// return Math.floor(Math.random() * max);
+// }
+// console.log(getNumber(numbers[39]));  
+// }
+// 
+// const numberPrompt = prompt("write the higher number shown in the console");
+// const correctNumber = ;
+// const wrongNumber = 1<correctNumber;
+// if (numberPrompt == correctNumber) {
+// console.log("true");
+// }
+// if (numberPrompt == wrongNumber) {
+// console.log("false");
+// }
+
+const selectNumbers = prompt("select 20 random numbers by writting them in the prompt");
+
+const selectedNumbers = [];
 
 for (let i = 0; i < 20; i++) {
 function getNumber(max) {
 return Math.floor(Math.random() * max);
 }
-console.log(getNumber(numbers[39]));  
+console.log(getNumber(selectedNumbers));  
 }
 
 const numberPrompt = prompt("write the higher number shown in the console");
 const correctNumber = ;
-const wrongNumber = 1<correctNumber;
-if (numberPrompt == correctNumber) {
-console.log("true");
-}
-if (numberPrompt == wrongNumber) {
-console.log("false");
-}
